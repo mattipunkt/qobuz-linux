ce56f69e4bf058cc6788175aa15b631660679938
mattipunkt <mattihro105@gmail.com>
2024-07-27 18:05:04 +0200

    building works

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ const createWindow = () => {
     const win = new BrowserWindow({
       width: 1200,
       height: 800,
-      icon: path.join(__dirname, "../icons/clearicon.png")
+      icon: path.join(__dirname, "../resources/icons/clearicon.png")
     })
     win.removeMenu()
     win.loadURL(qobuz_url)
