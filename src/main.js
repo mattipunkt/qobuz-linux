a39db1c5a5a79b5bfe2283cdd0f1246363a3534b
mattipunkt <mattihro105@gmail.com>
2024-07-28 09:40:18 +0200

    enable tray icon support with hiding

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu, Tray } = require('electron')
 var path = require('path');
+
+let appIcon = null
+let win = null
 
 const qobuz_url = "https://play.qobuz.com/"
 
 const createWindow = () => {
-    const win = new BrowserWindow({
+    win = new BrowserWindow({
       width: 1200,
       height: 800,
       icon: path.join(__dirname, "../resources/icons/clearicon.png")
@@ -20,9 +23,29 @@ app.on('window-all-closed', () => {
 })
 
 
+
 app.whenReady().then(() => {
+
     createWindow()
     
+    appIcon = new Tray(path.join(__dirname, "../resources/icons/clearicon.png"))
+    const contextMenu = Menu.buildFromTemplate([
+      { label: 'Quit', click() {
+        win.destroy()
+      } },
+      { label: 'Hide', click() {
+        win.hide()
+      }},
+      { label: 'Show', click() {
+        win.show()
+      }}
+    ])
+
+  
+    // Call this again for Linux because we modified the context menu
+    appIcon.setContextMenu(contextMenu)
+
+
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) createWindow()
     })
